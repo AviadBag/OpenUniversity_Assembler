9fac8961ec3db3c113cbbfe0e1996744d015692c
Aviad <aviad.bagno@gmail.com>
2021-05-03 21:40:18 +0300

    Implemented dictionary_create(0) and dictionary_put(3). (Not tested yet!)

---
+++ src/utils/dictionary.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <dictionary.h>
+
+/**
+ * An important note:
+ * In the outer interface for the function, I call "struct s_dictionary_node*" - "dictionary",
+ * in order to hide implementation details. But in the implementaion code, I just refer to that
+ * like a simple node pointer.
+ */
+
+typedef struct s_dictionary_node node;
+
+dictionary dictionary_create()
+{
+    return (node *)NULL; /* When head=null, the list is empty */
+}
+
+dictionary_error dictionary_put(node **nod, char *key, void *data)
+{
+    node *new_node = malloc(sizeof(node));
+    if (new_node == NULL)
+        return NOT_ENOUGH_MEMORY;
+
+    new_node->key = key;
+    new_node->data = data;
+
+    if (*nod == NULL)
+        *nod = new_node;
+    else
+    {
+        /* "tmp" holds the actual value of the head; Not a pointer to it. Makes life simpler. */
+        node *tmp = *nod;
+        /* Increment nod to point to the last node in the linked list. */
+        while (tmp->next != NULL)
+            tmp = tmp->next;
+
+        tmp->next = new_node;
+    }
+
+    return SUCCESS;
+}
