f98fd7f8935ef787a0066d2709ada3061aa614cc
Aviad <aviad.bagno@gmail.com>
2021-05-08 21:38:59 +0300

    Declared on the parser_parse(0) function - closes #10

--- include/parser.h
+++ include/parser.h
@@ -1,6 +1,8 @@
 #ifndef _PARSER_H
 #define _PARSER_H
 
+#include "status.h"
+
 typedef enum e_command_type
 {
     DIRECTIVE,  /* For example: .entry LABEL */
@@ -15,5 +17,13 @@ typedef struct s_command
     char *command_name;  /* The name of the command (For example - "addi") */
     char *label;         /* The label of that line; NULL if there is no label. */
 } command;
+
+/**
+ * Parses the given string.
+ * @param str The string to parse.
+ * @param cmd A pointer to where to store the parsed command.
+ * @return SYNTAX_ERROR or EMPTY_LINE or SUCCESS.
+ */
+status parser_parse(char* str, command* cmd);
 
 #endif
