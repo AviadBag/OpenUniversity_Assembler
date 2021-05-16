c29b6d0c885397fa133791adc85a34ab636acb51
Aviad <aviad.bagno@gmail.com>
2021-05-16 12:47:34 +0300

    Changed command.label to be an array instead of a pointer. Added PARSER_OVERFLOW to parser_status.

--- include/parser.h
+++ include/parser.h
@@ -1,6 +1,8 @@
 #ifndef _PARSER_H
 #define _PARSER_H
 
+#define LABEL_MAX_LENGTH 31
+
 typedef enum e_command_type
 {
     DIRECTIVE,  /* For example: .entry LABEL */
@@ -9,17 +11,18 @@ typedef enum e_command_type
 
 typedef struct s_command
 {
-    command_type type;   /* Tye type of the command */
-    char **operands;     /* All the operands of the command */
-    int operands_length; /* The length of the operands array */
-    char *command_name;  /* The name of the command (For example - "addi") */
-    char *label;         /* The label of that line; NULL if there is no label. */
+    command_type type;                /* Tye type of the command */
+    char **operands;                  /* All the operands of the command */
+    int operands_length;              /* The length of the operands array */
+    char *command_name;               /* The name of the command (For example - "addi") */
+    char label[LABEL_MAX_LENGTH + 1]; /* The label of that line; Empty string if there is no label. */
 } command;
 
 typedef enum e_parser_status
 {
     PARSER_SYNTAX_ERROR,
     PARSER_EMPTY_LINE,
+    PARSER_OVERFLOW, /* When the label is too long */
     PARSER_OK
 } parser_status;
 
@@ -27,8 +30,8 @@ typedef enum e_parser_status
  * Parses the given string.
  * @param str The string to parse.
  * @param cmd A pointer to a command struct to store inside the data.
- * @return PARSER_SYNTAX_ERROR or PARSER_EMPTY_LINE or PARSER_OK.
+ * @return PARSER_SYNTAX_ERROR or PARSER_EMPTY_LINE or PARSER_OVERFLOW or PARSER_OK.
  */
-parser_status parser_parse(char* str, command* cmd);
+parser_status parser_parse(char *str, command *cmd);
 
 #endif
