69c119dbed277addcf50bd39bc7057d9bcce881e
Aviad Bagno <aviad.bagno@gmail.com>
2021-05-22 21:26:29 +0300

    Declared on the validator module - closes #25

---
+++ include/validator.h
@@ -0,0 +1,23 @@
+#ifndef _VALIDATOR_H
+#define _VALIDATOR_H
+
+/**
+ * The validator module - validates that a given command is valid.
+ */
+
+#include "parser.h"
+
+typedef enum e_validator_status
+{
+    VALIDATOR_INVALID,
+    VALIDATOR_OK
+} validator_status;
+
+/**
+ * Checks if the given command is valid.
+ * @param cmd The command to check.
+ * @return VALIDATOR_INVALID or VALIDATOR_OK.
+ */
+e_validator_status validator_validate(command cmd);
+
+#endif
