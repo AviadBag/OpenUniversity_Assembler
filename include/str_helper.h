9a0f42f8186ee450838e90d989fa0b1c64d25d62
Aviad Bagno <aviad.bagno@gmail.com>
2021-05-26 19:19:18 +0300

    Created method has_whitespaces on str_helper module

--- include/str_helper.h
+++ include/str_helper.h
@@ -2,8 +2,17 @@
  * This module contains useful string functions.
  */
 
+#include "boolean.h"
+
 /**
  * Moves the given string to the first non-whitespace char
  * @param ptr A pointer to the string.
  */
 void skip_whitespaces(char** ptr);
+
+/**
+ * Cheks if the given string has whitespaces inside.
+ * @param str The string to check;
+ * @return true if it has; else - false.
+ */
+boolean has_whitespaces(char* str);
