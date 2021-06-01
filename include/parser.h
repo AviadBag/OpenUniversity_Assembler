d1d278b739b26abc0984d6abb36abd97fd446a40
avibag <aviad.bagno@gmail.com>
2021-06-01 15:40:55 +0300

    Added docs to all of the structs. - closes #39

--- include/parser.h
+++ include/parser.h
@@ -15,11 +15,11 @@ typedef enum e_command_type
 
 typedef struct s_command
 {
-    command_type type;                /* Tye type of the command */
-    char **operands;                  /* All the operands of the command. NULL if there are no operands. */
-    int number_of_operands;              /* The length of the operands array */
-    char *command_name;               /* The name of the command (For example - "addi") */
-    char label[LABEL_MAX_LENGTH + 1]; /* The label of that line; Empty string if there is no label. */
+    command_type type;                /**< Tye type of the command */
+    char **operands;                  /**< All the operands of the command. NULL if there are no operands. */
+    int number_of_operands;           /**< The length of the operands array */
+    char *command_name;               /**< The name of the command (For example - "addi") */
+    char label[LABEL_MAX_LENGTH + 1]; /**< The label of that line; Empty string if there is no label. */
 } command;
 
 typedef enum e_parser_status
