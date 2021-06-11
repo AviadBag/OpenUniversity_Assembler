f7ec153e87a6a8e34f9006549ebb421311ec3894
Aviad Bagno <aviad.bagno@gmail.com>
2021-06-11 13:46:32 +0300

    Updated validator.c to work with the new instructions_table - closes #46

--- src/main.c
+++ src/main.c
@@ -8,15 +8,12 @@
 void check_validator()
 {
     command cmd;
-    directive* d;
     parser_status p_status;
     validator_status v_status;
     
     printf("check_validator():\n");
 
-    instructions_table_init();
-
-    p_status = parser_parse("label:   add  $4, 5 ", &cmd, 17);
+    p_status = parser_parse("addd:   asdd  $4, 5 , 3", &cmd, 17);
     printf("%s\n", parser_status_to_string(p_status));
     if (command_has_label(cmd))
         printf("Label: %s\n", cmd.label);
@@ -24,8 +21,6 @@ void check_validator()
 
     v_status = validator_validate(cmd);
     printf("%s\n", validator_status_to_string(v_status));
-
-    instructions_table_free();
 }
 
 void check_directives_table()
@@ -38,6 +33,20 @@ void check_directives_table()
     s = directives_table_get_directive("db", &d);
     if (s == DT_OK)
         printf("Directive name = %s\n", d->name);
+    else
+        printf("ERROR\n");
+}
+
+void check_instructions_table()
+{
+    instruction* i;
+    instructions_table_status s;
+
+    printf("check_instructions_table():\n");
+
+    s = instructions_table_get_instruction("sw", &i);
+    if (s == IT_OK)
+        printf("Instruction name = %s\n", i->name);
     else
         printf("ERROR\n");
 }
