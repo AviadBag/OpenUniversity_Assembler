a73e6bfe8503595434f5ffabc2276afabd97fa4d
Aviad Bagno <aviad.bagno@gmail.com>
2021-07-12 12:18:36 +0300

    Printing the symbols on main.c (For debugging)

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 
 #include "first_walk.h"
 #include "linked_list.h"
+#include "symbol.h"
 #include "boolean.h"
 
 #define DESIRED_INPUT_FILE_EXT "as"
@@ -25,9 +26,24 @@ boolean has_legal_extension(char* file_name)
     return strcmp(dot, DESIRED_INPUT_FILE_EXT) == 0;
 }
 
-void print_symbol(void* value)
+void print_symbol(void* data)
 {
-    
+    symbol* s = (symbol*) (data);
+    printf("Symbol name: %s", s->name);
+    printf(", Symbol type: ");
+    switch (s->type)
+    {
+    case CODE:
+        printf("CODE");
+        break;
+    case DATA:
+        printf("DATA");
+        break;
+    case EXTERNAL:
+        printf("EXTERNAL");
+        break;
+    }
+    printf(", symbol is %s\n", s->is_entry ? "entry" : "not entry");
 }
 
 void print_symbols_table(symbols_table st)
