9969500574746fc7ebc4eb40ff9902818e9ccb27
Aviad Bagno <aviad.bagno@gmail.com>
2021-07-21 15:07:26 +0300

    Changed translator_translate() signature, to support getting a symbol table, and to be able to return errors.

--- include/translator.h
+++ include/translator.h
@@ -2,16 +2,25 @@
 #define _TRANSLATOR_H
 
 #include "command.h"
+#include "walk.h"
 
 /* This moudle translates a single assembly instruction to a 32 bit machine language instruction */
 
 typedef unsigned int machine_instruction; /* Represents an instruction in machine language */ 
 
+typedef enum e_translator_status
+{
+    TRANSLATOR_OK,
+    TRANSLATOR_LABEL_DOES_NOT_EXIST
+} translator_status;
+
 /**
  * This method translates the given command into a machine code.
- * @param cmd The command to translate. cmd.command_type MUST be INSTRUCTION, and must be validated by the validator.
+ * @param cmd                  The command to translate. cmd.command_type MUST be INSTRUCTION, and must be validated by the validator.
+ * @param st                   The symbols table.
+ * @param m                    A pointer to a machine_instruction; Will be filled with the instruction.
  * @return machine_instruction The translated instruction.
  */
-machine_instruction translator_translate(command cmd);
+translator_status translator_translate(command cmd, symbols_table st, machine_instruction* m);
 
 #endif
