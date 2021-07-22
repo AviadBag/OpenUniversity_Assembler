395838229ad9d2bbdc71a045c7e0b5edb017dcb4
Aviad Bagno <aviad.bagno@gmail.com>
2021-07-22 14:41:16 +0300

    Translator: Added label on J instructions, and applied prettier

--- src/translator.c
+++ src/translator.c
@@ -11,35 +11,36 @@
 
 #define R_COPY_INSTRUCTIONS_NUMBER_OF_OPERANDS 2
 
-#define RS_START      21
-#define RS_END        25
+#define RS_START 21
+#define RS_END 25
 
-#define RT_START      16
-#define RT_END        20
+#define RT_START 16
+#define RT_END 20
 
-#define RD_START      11
-#define RD_END        15
+#define RD_START 11
+#define RD_END 15
 
-#define OPCODE_START  26
-#define OPCODE_END    31
+#define OPCODE_START 26
+#define OPCODE_END 31
 
-#define FUNCT_START   6
-#define FUNCT_END     10
+#define FUNCT_START 6
+#define FUNCT_END 10
 
 #define IMMED_START 0
-#define IMMED_END   15
+#define IMMED_END 15
 
-#define REG_START     25
-#define REG_END       25
+#define REG_START 25
+#define REG_END 25
 
 #define ADDRESS_START 0
-#define ADDRESS_END   24
+#define ADDRESS_END 24
 
-#define I_INSTRUCTION_IMMED_SIZE_BYTES ((IMMED_END - IMMED_START + 1) / 8)
+#define I_INSTRUCTION_IMMED_SIZE_BITS (IMMED_END - IMMED_START + 1)
+#define J_INSTRUCTION_ADDRESS_SIZE_BITS (ADDRESS_END - ADDRESS_START + 1)
 
-#define TRANSLATOR        "Translator"
+#define TRANSLATOR "Translator"
 #define PROBLEM_WITH_CODE "ProblemWithCode"
-#define OVERFLOW          "Overflow"
+#define OVERFLOW "Overflow"
 
 /**
  * @brief Converts a register string (line "$3") to it's int representation. (Like 3).
@@ -47,7 +48,7 @@
  * @param register_str The register string. MUST be validated.
  * @return int         The register number.
  */
-static int register_string_to_int(char* register_str)
+static int register_string_to_int(char *register_str)
 {
 	return atoi(++register_str); /* Increment to skip the '$' */
 }
@@ -59,12 +60,12 @@ static int register_string_to_int(char* register_str)
  * @param cmd  The command to translate. MUST BE VALIDATED!
  * @param inst The instruction struct that represents the insturction.
  */
-void translate_R_instruction(machine_instruction* m, command cmd, instruction inst)
+void translate_R_instruction(machine_instruction *m, command cmd, instruction inst)
 {
 	int rs, rt, rd;
-	
+
 	bitmap_put_data(m, &inst.opcode, OPCODE_START, OPCODE_END); /* Put opcode */
-	bitmap_put_data(m, &inst.funct, FUNCT_START, FUNCT_END);   /* Put funct */
+	bitmap_put_data(m, &inst.funct, FUNCT_START, FUNCT_END);	/* Put funct */
 
 	if (inst.number_of_operands == R_COPY_INSTRUCTIONS_NUMBER_OF_OPERANDS)
 	{
@@ -96,11 +97,11 @@ void translate_R_instruction(machine_instruction* m, command cmd, instruction in
  * @param line                 On what line this instruction is?
  * @return translator_status TRANSLATOR_OK or TRANSLATOR_LABEL_DOES_NOT_EXIST or TRANSLATOR_OVERFLOW.
  */
-static translator_status translate_I_instruction(machine_instruction* m, command cmd, instruction inst, symbols_table st, int ic, int line)
+static translator_status translate_I_instruction(machine_instruction *m, command cmd, instruction inst, symbols_table st, int ic, int line)
 {
 	int rs, rt;
 	int immed;
-	symbol* symbol_p;
+	symbol *symbol_p;
 	int offset;
 
 	/* Put the opcode */
@@ -122,7 +123,7 @@ static translator_status translate_I_instruction(machine_instruction* m, command
 		}
 
 		offset = symbol_p->value - ic;
-		if (!is_in_range_2_complement(offset, I_INSTRUCTION_IMMED_SIZE_BYTES))
+		if (!is_in_range_2_complement(offset, I_INSTRUCTION_IMMED_SIZE_BITS))
 		{
 			logger_log(TRANSLATOR, OVERFLOW, line, "Label \"%s\" is too far!", cmd.operands[2]);
 			return TRANSLATOR_OVERFLOW;
@@ -131,14 +132,14 @@ static translator_status translate_I_instruction(machine_instruction* m, command
 	}
 	else
 	{
-		/* Arthimetic logic or memory instructions */ 
+		/* Arthimetic logic or memory instructions */
 		rs = register_string_to_int(cmd.operands[0]);
 		rt = register_string_to_int(cmd.operands[2]);
 		immed = atoi(cmd.operands[1]);
 	}
 
-	bitmap_put_data(m, &rs, RS_START, RS_END);   /* Put rs */
-	bitmap_put_data(m, &rt, RT_START, RT_END);  /* Put rt */
+	bitmap_put_data(m, &rs, RS_START, RS_END);			/* Put rs */
+	bitmap_put_data(m, &rt, RT_START, RT_END);			/* Put rt */
 	bitmap_put_data(m, &immed, IMMED_START, IMMED_END); /* Put immed */
 
 	return TRANSLATOR_OK;
@@ -151,32 +152,43 @@ static translator_status translate_I_instruction(machine_instruction* m, command
  * @param cmd                The command to translate. MUST BE VALIDATED!
  * @param inst               The instruction struct that represents the insturction.
  * @param st                 The symbols table.
+ * @param line               On what line this instruction is?
  * @return translator_status TRANSLATOR_OK or TRANSLATOR_LABEL_DOES_NOT_EXIST or TRANSLATOR_OVERFLOW.
  */
-static machine_instruction translate_J_instruction(machine_instruction* m, command cmd, instruction inst, symbols_table st)
+static machine_instruction translate_J_instruction(machine_instruction *m, command cmd, instruction inst, symbols_table st, int line)
 {
 	int reg = 0, address;
+	symbol *symbol_p;
 
 	/* Put the opcode */
 	bitmap_put_data(m, &inst.opcode, OPCODE_START, OPCODE_END);
-	
-	/* Here, every command is different */
-	if (strcmp(cmd.command_name, "jmp") == 0)
+
+	if (strcmp(cmd.command_name, "stop") == 0) /* The only J instruction that does not accept a label */
+		address = 0;
+	else
 	{
-		if (*cmd.operands[0] == '$') /* ThiS is a register */
+		if (*cmd.operands[0] == '$') /* This is a jmp instruction with a register */
 		{
 			reg = 1;
 			address = register_string_to_int(cmd.operands[0]);
 		}
 		else
-			address = 0; /* This is a label; The second walk will treat that. */
+		{
+			symbol_p = find_symbol(cmd.operands[0], st);
+			if (!symbol_p)
+			{
+				logger_log(TRANSLATOR, PROBLEM_WITH_CODE, line, "Label \"%s\" does not exist", cmd.operands[2]);
+				return TRANSLATOR_LABEL_DOES_NOT_EXIST;
+			}
+
+			address = symbol_p->value;
+			if (!is_in_range_2_complement(address, I_INSTRUCTION_IMMED_SIZE_BITS))
+			{
+				logger_log(TRANSLATOR, OVERFLOW, line, "Label \"%s\" is too far!", cmd.operands[2]);
+				return TRANSLATOR_OVERFLOW;
+			}
+		}
 	}
-	else
-		/* This is either:
-		   1. "call" or "la" - and then address should be zero, and the second walk will treat it.
-		   2. "stop"         - and then there are no operands, so address should be 0.
-		*/
-		address = 0;
 
 	bitmap_put_data(m, &address, ADDRESS_START, ADDRESS_END);
 	bitmap_put_data(m, &reg, REG_START, REG_END);
@@ -184,17 +196,17 @@ static machine_instruction translate_J_instruction(machine_instruction* m, comma
 	return TRANSLATOR_OK;
 }
 
-translator_status translator_translate(command cmd, symbols_table st, int ic, int line, machine_instruction* m)
+translator_status translator_translate(command cmd, symbols_table st, int ic, int line, machine_instruction *m)
 {
-	instruction* inst;
+	instruction *inst;
 	instructions_table_get_instruction(cmd.command_name, &inst);
 	*m = 0;
-	if (inst->type == R)	
+	if (inst->type == R)
 		translate_R_instruction(m, cmd, *inst);
 	else if (inst->type == I)
 		return translate_I_instruction(m, cmd, *inst, st, ic, line);
 	else /* J */
-		return translate_J_instruction(m, cmd, *inst, st);
+		return translate_J_instruction(m, cmd, *inst, st, line);
 
 	return TRANSLATOR_OK;
 }
