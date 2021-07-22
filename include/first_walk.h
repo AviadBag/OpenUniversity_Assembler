22e56c5729c38ab896449ea3c346a5fac96bf561
Aviad Bagno <aviad.bagno@gmail.com>
2021-07-22 16:40:08 +0300

    Added comment that reminds to free the memory, in the description of every function that allocates memory. - closes #92

--- include/first_walk.h
+++ include/first_walk.h
@@ -8,7 +8,7 @@
  * @brief This method does the first walk - creates a symbols table from the given file.
  * 
  * @param file_name The file to compile.
- * @param symbols_table Will point to the symbol table. SHOULD BE FREED BY linked_list_free().
+ * @param symbols_table Will point to the symbol table. SHOULD BE FREED BY linked_list_free(), and it's symbols should be freed.
  * @return walk_status WALK_IO_ERROR or WALK_NOT_ENOUGH_MEMORY or WALK_PROBLEM_WITH_CODE or WALK_OK.
  */
 walk_status first_walk(char* file_name, symbols_table* symbols_table_p);
